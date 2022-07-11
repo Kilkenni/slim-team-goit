8dbe9d89d211fb8e129873a0b102edeb4834c549
o-ishchenko <olya.galaychuk@gmail.com>
2022-07-11 19:35:25 +0300

    Add header without styles

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,12 @@
-import { Routes, Route } from 'react-router-dom';
-import { lazy, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { authOperations, authSelectors } from '../redux/auth';
-import Layout from './Layout';
-import PrivateRoute from './PrivateRoute';
-import PublicRoute from './PublicRoute';
+import { Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { authOperations, authSelectors } from "../redux/auth";
+import Layout from "./Layout";
+import PublicRoute from "./PublicRoute";
 
-import LoginPage from '../pages/LoginPage';
-import RegisterPage from '../pages/RegisterPage';
-
-
+import LoginPage from "../pages/LoginPage";
+import RegisterPage from "../pages/RegisterPage";
 
 function App() {
   const dispatch = useDispatch();
@@ -18,20 +15,30 @@ function App() {
   useEffect(() => {
     dispatch(authOperations.fetchCurrentUser());
   }, [dispatch]);
-  
+
   return (
     <Routes>
-      {!isFetchingCurrentUser &&
+      {!isFetchingCurrentUser && (
         <Route path="/" element={<Layout />}>
-        
-        <Route path="register" element={<PublicRoute ><RegisterPage /></PublicRoute>} />
-        <Route path="login" element={<PublicRoute ><LoginPage /></PublicRoute>} />       
-        
+          <Route
+            path="register"
+            element={
+              <PublicRoute>
+                <RegisterPage />
+              </PublicRoute>
+            }
+          />
+          <Route
+            path="login"
+            element={
+              <PublicRoute>
+                <LoginPage />
+              </PublicRoute>
+            }
+          />
         </Route>
-      }
-    </Routes>  
-
-    
+      )}
+    </Routes>
   );
 }
 
