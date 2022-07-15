c2daa305c5c4ed5c85e3445f9ef0405e38551dff
AleksandrF93 <86001296+AleksandrF93@users.noreply.github.com>
2022-07-16 00:19:08 +0300

    Calendar Fix

--- src/pages/DiaryPage/DiaryPageView.js
+++ src/pages/DiaryPage/DiaryPageView.js
@@ -2,20 +2,21 @@ import Container from "../../components/Container";
 import DiaryDateСalendar from '../../components/DiaryDateСalendar/DiaryDateСalendar';
 import LeftSideBar from '../../components/LeftSideBar';
 import RightSideBar from '../../components/RightSideBar';
-
+import { useState } from 'react';
 import DiaryProductList from '../../components/DiaryPtoductList';
 import DiaryProductForm from '../../components/DiaryPtoductForm';
 
 export default function DiaryPageView() {
+    const [date, setDate] = useState(new Date());
 
     return (
-        <Container>
+        <Container date={date}>
             <LeftSideBar>
-                <DiaryDateСalendar />
+                <DiaryDateСalendar onChangeDate={setDate} date={date} />
                 <DiaryProductForm />
                 <DiaryProductList />
             </LeftSideBar>
-            <RightSideBar/>
+            <RightSideBar date={date} />
         </Container>
     );
 }
