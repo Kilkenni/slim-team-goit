d7e2c5fb4e0db42530a036957497a85a8b124c46
blazhko22 <86318145+blazhko22@users.noreply.github.com>
2022-07-16 17:14:34 +0300

    diatyProduct

--- src/components/DiaryPtoductList/DiaryProductList.jsx
+++ src/components/DiaryPtoductList/DiaryProductList.jsx
@@ -2,14 +2,21 @@ import React from "react";
 import DiaryProductItem from '../DiaryPtoductItem/DiaryProductItem';
 import s from './DiaryProductList.module.scss';
 
-const DiaryProductList = () => {
+const DiaryProductList = ({setVisibleForm, setVisibleList}) => {
+
+  const handleChange = () => {
+    setVisibleForm(true)
+    setVisibleList(false)
+  }
 
   return (
+    
     <div className={s.block}>
+
       <ul className={s.list}>
           <DiaryProductItem />
       </ul>
-      <button className={s.button}>
+      <button onClick={handleChange} className={s.button}>
         <span className={s.plus}>+</span>
       </button>
     </div>
