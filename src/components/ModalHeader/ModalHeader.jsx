478dcf434d8833b46293d5afe0f79b7c9aa651b7
o-ishchenko <olya.galaychuk@gmail.com>
2022-07-16 19:32:46 +0300

    Вносить правки
    -Поправляє ширину Layout для моб.версії
    -Правки в модальному вікні (додано хедер) і кнопку закриття

--- src/components/ModalHeader/ModalHeader.jsx
+++ src/components/ModalHeader/ModalHeader.jsx
@@ -1,6 +1,10 @@
 import { createPortal } from "react-dom";
 import styles from "./ModalHeader.module.scss";
 import { useEffect } from "react";
+import { useMediaQuery } from "../../js/hooks";
+import btnClose from "./btnClose.svg";
+import Logo from "../Logo"
+import UserMenu from "../UserMenu";
 
 const modalRoot = document.querySelector("#modal-root");
 
@@ -10,6 +14,8 @@ const modalRoot = document.querySelector("#modal-root");
  * @returns Модальне вікно з контентом, переданим як діти. В мобільній версії автоматично включає в себе Header.
  */
 function ModalHeader({ onClose, children, ...otherProps }) {
+  const tabletSize = getComputedStyle(document.documentElement).getPropertyValue("--breakpoint-tablet");
+  const isMobile = useMediaQuery(`(max-width: ${tabletSize})`);
 
   function closeOnBackdrop(event) {
     if (event.target !== event.currentTarget) {
@@ -33,6 +39,15 @@ function ModalHeader({ onClose, children, ...otherProps }) {
   return createPortal(
     <div className={styles.backdrop} onClick={closeOnBackdrop}>
       <div className={styles.modal}>
+        <div className={styles.header}>
+          <Logo onClick={onClose}/>
+          {!isMobile && <UserMenu />}
+          <button type="button" className={styles.button} onClick={onClose}>
+            <svg alt="menu icon" width="18" height="18" className={styles.icon}>
+              <use href={`${btnClose}#btnClose`}></use>
+            </svg>
+          </button>
+        </div>
         <div className={styles.contentBlock}>
           {children}
         </div>   
