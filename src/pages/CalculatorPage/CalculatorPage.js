d361c47e44949f886fcb42a68bf97a23d605e3f4
AleksandrF93 <86001296+AleksandrF93@users.noreply.github.com>
2022-07-16 21:56:03 +0300

    RightSideBar Fix
    добавил сегодняшнюю дату в RightSideBar с калькулятором

--- src/pages/CalculatorPage/CalculatorPage.js
+++ src/pages/CalculatorPage/CalculatorPage.js
@@ -1,16 +1,17 @@
 import DailyCaloriesForm from '../../components/DailyCaloriesForm/DailyCaloriesForm';
 import RightSideBar from '../../components/RightSideBar/RightSideBar';
 import styles from "./CalculatorPage.module.scss";
-
+import { useState } from "react";
 
 const CalculatorPage = () => {
+  const [date, setDate] = useState(new Date());
   return (
     <div className={styles.container}>
     <div className={styles.container_calculator}>
     <h1 className={styles.container__title}>Розрахуйте добову норму калорій прийом прямо зараз</h1>
       <DailyCaloriesForm />
       </div>
-      <RightSideBar/>
+      <RightSideBar date={date} />
     </div>
 
   
