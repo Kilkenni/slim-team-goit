8143ac75b63842085ce1abad95c0aa0ee4e9b1d0
o-ishchenko <olya.galaychuk@gmail.com>
2022-07-19 12:20:19 +0300

    add deleting

---
+++ src/components/Loader/Loader.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { CircleLoader } from 'react-spinners';
+
+function Loader() {
+  const accentColor = getComputedStyle(
+    document.documentElement
+  ).getPropertyValue('--accent-main');
+  const loaderCSSOverride = {
+    marginTop: '100px',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+  };
+  return (
+    <CircleLoader
+      color={accentColor}
+      size={150}
+      cssOverride={loaderCSSOverride}
+    />
+  );
+}
+
+export default Loader;
