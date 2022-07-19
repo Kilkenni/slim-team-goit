9c05cb1405b05113d0ec350661630c17126118ba
o-ishchenko <olya.galaychuk@gmail.com>
2022-07-19 13:46:08 +0300

    add navigate
    не робочий варіант :(

--- src/components/UserMenu/UserMenu.js
+++ src/components/UserMenu/UserMenu.js
@@ -1,24 +1,31 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { authSelectors, authOperations } from '../../redux/auth';
+// import { useNavigate, NavLink } from 'react-router-dom';
 
 import styles from './UserMenu.module.scss';
 
 export default function UserMenu({ ...DOMprops }) {
   const dispatch = useDispatch();
   const name = useSelector(authSelectors.getUsername);
+  // const navigate = useNavigate();
 
   return (
     <div className={styles.container}>
       <span className={styles.name}>{name}</span>
-
+      {/* <NavLink to="/login"> */}
       <button
         className={styles.button}
         type="button"
-        onClick={() => dispatch(authOperations.logOut())}
+        onClick={() => {
+          dispatch(authOperations.logOut());
+          // navigate('/login')
+        }}
         {...DOMprops}
       >
         Вийти
       </button>
+
+      {/* </NavLink> */}
     </div>
   );
 }
