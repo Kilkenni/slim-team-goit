cdbdbfd74eee60bd01ccc309649511d25cae3b00
Askarunro <paskar.y.a@gmail.com>
2022-07-19 04:43:56 +0300

    Update DiaryPageView.js

--- src/pages/DiaryPage/DiaryPageView.js
+++ src/pages/DiaryPage/DiaryPageView.js
@@ -1,22 +1,36 @@
-import Container from "../../components/Container";
+import Container from '../../components/Container';
 import DiaryDateСalendar from '../../components/DiaryDateСalendar/DiaryDateСalendar';
 import LeftSideBar from '../../components/LeftSideBar';
 import RightSideBar from '../../components/RightSideBar';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import DiaryProduct from '../../components/DiaryProduct';
 import DiaryProductList from '../../components/DiaryPtoductList';
+import { getProductsDiary } from '..//../js/backendAPI';
 
 export default function DiaryPageView() {
-    const [date, setDate] = useState(new Date());
-    const [item, setItem] = useState();
-    return (
-        <Container date={date}>
-            <LeftSideBar>
-                <DiaryDateСalendar onChangeDate={setDate} date={date} />
-                <DiaryProduct />
-                <DiaryProductList/>
-            </LeftSideBar>
-            <RightSideBar date={date} />
-        </Container>
+  const [date, setDate] = useState(new Date());
+  const [item, setItem] = useState();
+  const [items, setItems] = useState();
+  const dateCurrent = new Date(date).toLocaleDateString().replace(/\./g, '.');
+  // useEffect(()=>{
+  //     getProductsDiary(dateCurrent).then(setItems)
+  // },[item])
+  // getProductsDiary(dateCurrent).then(setItems)
+
+  useEffect(() => {
+    getProductsDiary(dateCurrent).then(response =>
+      setItems(response.data.data.productList)
     );
+  }, [dateCurrent, item]);
+  console.log(items);
+  return (
+    <Container date={date}>
+      <LeftSideBar>
+        <DiaryDateСalendar onChangeDate={setDate} date={date} />
+        <DiaryProduct setItem={setItem} date={dateCurrent} />
+        <DiaryProductList newItem={items} />
+      </LeftSideBar>
+      <RightSideBar date={date} />
+    </Container>
+  );
 }
